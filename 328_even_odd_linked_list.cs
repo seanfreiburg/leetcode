78d6b976fee72f9f71c8e9dea1a5bae54932ef17
Sean Freiburg <seanfreiburg@gmail.com>
2017-06-15 01:28:56 -0700

    Create 328_even_odd_linked_list.cs

---
+++ 328_even_odd_linked_list.cs
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode OddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
